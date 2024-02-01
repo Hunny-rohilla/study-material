50b0d586a70e2c741dccf17c000dec6ef56fbbcf
Hoshiyar <harshrohilla0001@gmail.com>
2024-02-01 20:22:56 +0530

    added leet code problem and solution

--- techStack/JavaScript/leetCodeProblems.js
+++ techStack/JavaScript/leetCodeProblems.js
@@ -15,7 +15,7 @@ var restoreString = function(s, indices) {
 };
 
 // -------------------------------------------------------------------------------------------
-// problem 2:
+// problem 2: Easy	https://leetcode.com/problems/happy-number/
 // Write an algorithm to determine if a number n is happy.
 
 // A happy number is a number defined by the following process:
@@ -58,5 +58,31 @@ var isHappy = function(n) {
         }
     }
 };
+
+// -------------------------------------------------------------------------------------------
+
+// problem 3: Easy	https://leetcode.com/problems/two-sum/
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+// Example 1:
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+// solution:
+var twoSum = function(nums, target) {
+    const numIndicesMap = {};
+  
+    for (let i = 0; i < nums.length; i++) {
+      const complement = target - nums[i];
+      if (complement in numIndicesMap) {
+        return [numIndicesMap[complement], i];
+      }
+      numIndicesMap[nums[i]] = i;
+    }
+  
+  };
 
 // -------------------------------------------------------------------------------------------
