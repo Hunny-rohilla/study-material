344894ea502fe0b928c091458e9477efb6f886f5
Hoshiyar <harshrohilla0001@gmail.com>
2024-04-06 23:12:26 +0530

    added sql interview questions

--- techStack/SQL/Notes.js
+++ techStack/SQL/Notes.js
@@ -2,3 +2,16 @@
 //? https://www.interviewbit.com/sql-interview-questions/
 //? https://www.postgresqltutorial.com/
 //? https://www.youtube.com/watch?v=85pG_pDkITY
+
+//================================================================
+//## SQL Interview Questions
+//================================================================
+
+//# 1. What is Database?
+// A database is an organized collection of data, stored and retrieved digitally from a remote or local computer system. Databases can be vast and complex, and such databases are developed using fixed design and modeling approaches.
+
+//# 2. What is DBMS?
+// DBMS stands for Database Management System. DBMS is a system software responsible for the creation, retrieval, updation, and management of the database. It ensures that our data is consistent, organized, and is easily accessible by serving as an interface between the database and its end-users or application software.
+
+//# 3. What is RDBMS? How is it different from DBMS?
+// RDBMS stands for Relational Database Management System. The key difference here, compared to DBMS, is that RDBMS stores data in the form of a collection of tables, and relations can be defined between the common fields of these tables. Most modern database management systems like MySQL, Microsoft SQL Server, Oracle, IBM DB2, and Amazon Redshift are based on RDBMS.
